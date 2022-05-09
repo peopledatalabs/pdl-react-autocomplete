e7087560fecd5c086fbb67c0d799590aa4a3fc07
David Oh <103519873+davidoh15@users.noreply.github.com>
2022-05-09 15:09:33 -0700

    clean up

--- example/src/index.js
+++ example/src/index.js
@@ -6,10 +6,6 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-require('react-dom');
-window.React2 = require('react');
-console.log(window.React1 === window.React2);
-
 root.render(
   <React.StrictMode>
     <div className="App">
